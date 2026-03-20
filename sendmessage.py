24dba92345fd706ef6f55155c1226b6b44a036ce
Allan Jales <jales.allan@gmail.com>
2026-03-20 20:53:39 +0100

    HTML format to message

--- sendmessage.py
+++ sendmessage.py
@@ -21,8 +21,13 @@ def load_env(filename=".env"):
     return env_vars
 
 def send_telegram_message(token, chat_id, text):
+    text = text.replace("\\n", "\n")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
-    payload = {"chat_id": chat_id, "text": text}
+    payload = {
+            "chat_id": chat_id,
+            "text": text,
+            "parse_mode": "HTML"
+    }
     response = requests.post(url, data=payload)
     
     if response.status_code == 200:
